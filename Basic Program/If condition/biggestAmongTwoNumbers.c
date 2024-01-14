ecf8a284b77e66ba39eebe88c239b748a004b174
karthik B <82690268+karthik-noveau@users.noreply.github.com>
2024-01-14 21:17:15 +0000

    c programs

---
+++ Basic Program/If condition/biggestAmongTwoNumbers.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num = 10;
+
+    if (num % 10 == 0)
+    {
+        printf("Even number");
+    }
+    else
+    {
+        printf("Odd number");
+    }
+}
+
+
---
+++ Basic Program/demo.c
@@ -0,0 +1,21 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    sum();
+}
+
+
+
+sum()
+{
+    int value1 = 10;
+    int value2 = 20;
+
+    int add = value1 + value2;
+    printf("%d", add);
+}
+
+
